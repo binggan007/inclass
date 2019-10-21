17936ba8e2d93b57a1c5ee7d6ef5df7dcbabadf9
Yu <yufu0012016@gmail.com>
2019-10-20 22:13:20 -0400

    9 mutants alive and could not figure out why. Mutation score 94%

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -107,6 +107,103 @@ public class TriangleTest {
         assertEquals(actual, expected);
     }
 
+      @Test
+    // mutation test for isosceles
+    public void testIsosceles(){
+	// triangle type: scalene, isosceles, equilateral
+	// condition: triple negative, single negative, double negative, inequality
+	// a = b, valid isosceles
+	assertEquals(Triangle.classify(3,3,5), ISOSCELES);
+	// a = c, valid isosceles
+	assertEquals(Triangle.classify(5,3,5), ISOSCELES);
+	// b = c, valid isosceles
+        assertEquals(Triangle.classify(3,5,5), ISOSCELES);
+   	// a = b, but a+b=c
+	assertEquals(Triangle.classify(3,3,6), INVALID);
+	// a = c, but a+c=b
+	assertEquals(Triangle.classify(3,6,3), INVALID);
+	// b = c, but b+c=a
+	assertEquals(Triangle.classify(6,3,3), INVALID);
+	// a = b, but a+b<c
+	assertEquals(Triangle.classify(3,3,7), INVALID);
+	// a = c, but a+c<b
+	assertEquals(Triangle.classify(3,7,3), INVALID);
+	// b = c, but b+c<a
+	assertEquals(Triangle.classify(7,3,3), INVALID);
+
+    }
+  
+
+   @Test
+   // mutation test for EQUILATERAL
+    public void testEquilateral(){
+	// triangle type: scalene, isosceles, equilateral
+	// condition: triple negative, single negative, double negative, inequality
+	// valid equilateral
+	assertEquals(Triangle.classify(6,6,6), EQUILATERAL);
+	// all negative equilateral
+	assertEquals(Triangle.classify(-6,-6,-6), INVALID);
+	// double negative equilateral
+	assertEquals(Triangle.classify(6,-6,-6), INVALID);
+	// double negative equilateral
+	assertEquals(Triangle.classify(-6,6,-6), INVALID);
+	// double negative equilateral
+	assertEquals(Triangle.classify(-6,-6,6), INVALID);
+	// single negative equilateral
+	assertEquals(Triangle.classify(6,6,-6), INVALID);
+	// single negative equilateral
+	assertEquals(Triangle.classify(6,-6,6), INVALID);
+	// single negative equilateral
+	assertEquals(Triangle.classify(-6,6,6), INVALID);
+	// small length
+	assertEquals(Triangle.classify(1,1,1), EQUILATERAL);
+	// zero length
+	assertEquals(Triangle.classify(0,1,1), INVALID);
+	// zero length
+	assertEquals(Triangle.classify(1,0,1), INVALID);
+	// zero length
+	assertEquals(Triangle.classify(1,1,0), INVALID);
+    }
+
+   
+   @Test
+   // mutation test for SCALENE
+    public void testSCALENE(){
+   	// regular triangle
+	assertEquals(Triangle.classify(3,4,5),SCALENE);
+	// triple negative regular triangle
+	assertEquals(Triangle.classify(-3,-4,-5),INVALID);
+	// single negative regular triangle
+	assertEquals(Triangle.classify(-3,4,5),INVALID);
+	// single negative regular triangle
+	assertEquals(Triangle.classify(3,-4,5),INVALID);
+	// single negative regular triangle
+	assertEquals(Triangle.classify(3,4,-5),INVALID);
+	// double negative regular triangle
+	assertEquals(Triangle.classify(3,-4,-5),INVALID);
+	// double negative regular triangle
+	assertEquals(Triangle.classify(-3,4,-5),INVALID);
+	// double negative regular triangle
+	assertEquals(Triangle.classify(-3,-4,5),INVALID);
+	// a+b<c
+	assertEquals(Triangle.classify(3,4,8), INVALID);
+	// a+c<b
+	assertEquals(Triangle.classify(3,8,4), INVALID);
+	// b+c<a
+	assertEquals(Triangle.classify(8,4,3), INVALID);  
+	// a+b=c
+	assertEquals(Triangle.classify(3,4,7), INVALID);
+	// a+c=b
+	assertEquals(Triangle.classify(3,7,4), INVALID);
+	// b+c=a
+	assertEquals(Triangle.classify(7,4,3), INVALID);  
+	//mutant 76
+	//mutant 11 not caught
+	//mutant 80
+	assertEquals(Triangle.classify(91,10,100),SCALENE);
+	assertEquals(Triangle.classify(100,10,91),SCALENE);
+	assertEquals(Triangle.classify(100,91,10),SCALENE);
+   }
 
 }
 
